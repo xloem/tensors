e47f9d2b7bddc2f29e44d12eeb3f67db57fd0bfa
0xloem <0xloem@gmail.com>
2021-10-24 06:48:24 -0400

    parameterise generation paths

--- sphinx/generate.sh
+++ sphinx/generate.sh
@@ -1,2 +1,4 @@
 SCRIPT_PATH="`dirname "$0"`"
-sphinx-build -b gen -c "$SCRIPT_PATH" "$SCRIPT_PATH"/../array-api/spec/API_specification "$SCRIPT_PATH"/../generated
+INPUT_PATH="$SCRIPT_PATH"/../array-api/spec/API_specification
+OUTPUT_PATH="$SCRIPT_PATH"/../generated
+sphinx-build -b gen -c "$SCRIPT_PATH" "$INPUT_PATH" "$OUTPUT_PATH"
